98a32f9e10a8c3119d49363a2b2170cd97c32c9f
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2021-06-21 00:30:08 +0530

    ui: bug fix: install react-router dependency and added React to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CameraComponent from './views/camera';
 import Gallery from './views/gallery';
